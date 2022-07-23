57b31e2eae0ed56e3aeb3c1902f6876151f2440c
chamsBLT <61758859+chamsBLT@users.noreply.github.com>
2022-07-24 00:52:37 +0100

    Update subject.service.ts

--- src/app/Services/subject.service.ts
+++ src/app/Services/subject.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SubjectService {
 
-  readonly API_URL = 'http://localhost:8082/internship-app/subjects';
+  readonly API_URL = 'http://internship-back-svc:8082/internship-app/subjects';
 
   constructor(private httpClient: HttpClient) { }
 
